bf25bf37b2d253f5a9c6ec6ae347df3243431c3a
zackyj <zackyjz@qq.com>
2021-06-29 16:25:24 +0800

    集成 mybatis

--- src/main/java/com/zackyj/wiki/controller/TestController.java
+++ src/main/java/com/zackyj/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zackyj.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "ok";
+    }
+
+    @PostMapping("/hi")
+    public String hi(String name){
+        return "hi"+name;
     }
 }
