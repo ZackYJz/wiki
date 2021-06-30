65362f3a11b19303ac03db9f863cd1462b77dd2d
zackyj <zackyjz@qq.com>
2021-06-30 13:25:47 +0800

    统一响应对象

---
+++ src/main/java/com/zackyj/wiki/model/resp/CommonResp.java
@@ -0,0 +1,80 @@
+package com.zackyj.wiki.model.resp;
+
+public class CommonResp<T> {
+
+    /**
+     * 业务上的成功或失败
+     */
+    private boolean success = true;
+
+    /**
+     * 返回信息
+     */
+    private String message;
+
+    /**
+     * 返回泛型数据，自定义类型
+     */
+    private T content;
+
+    public CommonResp(){
+        this(true,"ok",null);
+    }
+    public CommonResp(T content){
+        this(true,null,content);
+    }
+
+    public CommonResp(boolean success,String message){
+        this.success = success;
+        this.message = message;
+    }
+
+    public CommonResp(boolean success, String message, T content) {
+        this.success = success;
+        this.message = message;
+        this.content = content;
+    }
+
+    //返回成功结果
+    public static <T> CommonResp<T> success(){
+        return new CommonResp<>();
+    }
+
+    public static <T> CommonResp<T> success(T content){
+        return new CommonResp<>(content);
+    }
+    //返回失败结果
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ResponseDto{");
+        sb.append("success=").append(success);
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", content=").append(content);
+        sb.append('}');
+        return sb.toString();
+    }
+}
