32bb26790e50c0e2998343c7cdfabc2fc0d370ff
zackyj <zackyjz@qq.com>
2021-06-30 15:12:15 +0800

    集成对象列表拷贝工具类

--- src/main/java/com/zackyj/wiki/service/EbookService.java
+++ src/main/java/com/zackyj/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.zackyj.wiki.mapper.EbookMapper;
 import com.zackyj.wiki.model.pojo.Ebook;
 import com.zackyj.wiki.model.req.EbookReq;
 import com.zackyj.wiki.model.resp.EbookResp;
+import com.zackyj.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,12 +25,7 @@ public class EbookService {
 
     public List<EbookResp> list(EbookReq ebookReq) {
         List<Ebook> ebookList = ebookMapper.selectByName(ebookReq.getName());
-        List<EbookResp> ebookRespList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            ebookRespList.add(ebookResp);
-        }
+        List<EbookResp> ebookRespList = CopyUtil.copyList(ebookList, EbookResp.class);
         return ebookRespList;
     }
 }
