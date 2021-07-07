213d91f230b8d10d48bebf5818918acd89fd47d0
zackyj <zackyjz@qq.com>
2021-07-07 15:09:56 +0800

    抽取自定义the-header 组件

--- vue-wiki/src/App.vue
+++ vue-wiki/src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <a-layout-content style="padding: 0 50px">
       <a-breadcrumb style="margin: 16px 0">
         <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -27,8 +15,19 @@
   </a-layout>
 </template>
 
-<style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
 
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+  },
+});
+</script>
+
+<style>
 #components-layout-demo-top-side .logo {
   float: left;
   width: 120px;
