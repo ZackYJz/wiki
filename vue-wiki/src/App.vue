8b81bef1970c888e32736c5f0b7564e8593ec74d
zackyj <zackyjz@qq.com>
2021-07-07 15:17:29 +0800

    抽取自定义the-footer 组件

--- vue-wiki/src/App.vue
+++ vue-wiki/src/App.vue
@@ -2,27 +2,22 @@
   <a-layout>
     <the-header></the-header>
     <a-layout-content style="padding: 0 50px">
-      <a-breadcrumb style="margin: 16px 0">
-        <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
-      </a-breadcrumb>
-        <router-view/>
+      <router-view/>
     </a-layout-content>
-    <a-layout-footer style="text-align: center">
-      wiki footer
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter,
   },
 });
 </script>
---
+++ vue-wiki/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    wiki footer 11111
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+  name: "the-footer"
+};
+</script>
+
+<style scoped>
+
+</style>
