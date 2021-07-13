b904072b37932d1963e74ad7c2dfa1d6f29595c4
zackyj <zackyjz@qq.com>
2021-07-13 08:43:01 +0800

    修改baseURL

--- vue-wiki/src/main.ts
+++ vue-wiki/src/main.ts
@@ -5,6 +5,10 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icon from '@ant-design/icons-vue';
+import axios from 'axios';
+
+//axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+axios.defaults.baseURL = 'http://localhost:8081';
 
 const app = createApp(App);
 app.use(store).use(router)
@@ -16,3 +20,6 @@ const icons:any= Icon;
 for(const i in icons){
     app.component(i,icons[i]);
 }
+
+console.log("环境:",process.env.NODE_ENV);
+console.log("当前服务端:",process.env.VUE_APP_SERVER);
--- vue-wiki/src/views/Home.vue
+++ vue-wiki/src/views/Home.vue
@@ -95,7 +95,7 @@ export default defineComponent({
 
     //初始化方法
     onMounted(()=>{
-      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) => {
+      axios.get("/ebook/list").then((response) => {
         const data=response.data;
         ebooks.value = data.content;
         console.log(response);
