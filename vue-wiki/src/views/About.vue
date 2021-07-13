92addd9cc03cb82b8eeea7303c5c6da0f8bf67b3
zackyj <zackyjz@qq.com>
2021-07-13 20:16:45 +0800

    基本框架

--- vue-wiki/src/views/About.vue
+++ vue-wiki/src/views/About.vue
@@ -1,4 +1,28 @@
 <template>
+  <a-layout style="padding: 24px 0; background: #fff">
+    <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
+      <a-list item-layout="vertical" size="large"
+              :data-source="ebooks" :grid="{gutter:15,column:3}">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
+    </a-layout-content>
+  </a-layout>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
